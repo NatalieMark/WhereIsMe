3dcd0f9ad0f4e29bc910739cbfe795b2f46121a1
Natalie Mark <natalies2005@hotmail.com>
2018-03-26 20:35:22 +0200

    no message

---
+++ Where is me/Assets/Script/Controls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Left(int speed)
+    {
+		if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+	}
+
+    public void Right(int speed)
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+            
+            
+	}
+}
---
+++ Where is me/Assets/Script/Test_Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Move : MonoBehaviour {
+
+    public Controls controls;
+    public float StartPosX;
+    public float StartPosY;
+    public int Speed;
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = new Vector2(StartPosX, StartPosY);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Left(Speed);
+        Right(Speed);
+	}
+
+	public void Left(int speed)
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(Vector2.left * speed * Time.deltaTime);
+		}
+	}
+
+	public void Right(int speed)
+	{
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(Vector2.right * speed * Time.deltaTime);
+		}
+	}
+
+}
